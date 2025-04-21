448d9b603ff79373e0928c143912262e61958e0f
Richelin Metellus <richelin.metellus@outlook.com>
2025-04-21 10:45:35 -0500

    created a simple program that will greet the user by name

--- _resources/Examples/tut-001_basic-hello/src/main.rs
+++ _resources/Examples/tut-001_basic-hello/src/main.rs
@@ -1,12 +1,12 @@
-#![allow(unused)]
+// #![allow(unused)]
 
 use std:: io;
-use rand::Rng;
-
-use std::io::{Write, BufReader, BufRead, ErrorKind};
-use std::fs::File;
-use std::cmp::Ordering;
 
 fn main() {
-    println!("Hello, world!");
+    println!("what is your name?");
+    let mut name = String::new();
+    let greeting: &str = "Nice to meet you";
+    io::stdin().read_line(&mut name)
+        .expect("Didn't Receive User Input");
+    println!("Hello {}! {}", name.trim_end(), greeting);
 }
