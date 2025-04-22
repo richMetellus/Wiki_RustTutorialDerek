34ca631ace0732a3a5063dd30b5102835ff69cc9
Richelin Metellus <richelin.metellus@outlook.com>
2025-04-22 15:24:25 -0500

    test new exercise that show the different scalar data types in rust and some examples to show their max

--- _resources/Examples/tut-002_var-datatype-ops/src/main.rs
+++ _resources/Examples/tut-002_var-datatype-ops/src/main.rs
@@ -1,7 +1,9 @@
 // constant variables demo code:
 
 fn main() {
-    const ONE_MIL: u32 = 1_000_000;
+    println!("-------------------------------------------------------------------");
+
+    const ONE_MIL: u32 = 1_000_000; // need explicit type annotation
     const PI : f32 = 3.141592;
     let age = "47"; // a string, implicitly typed. defined with double quotes
     let mut age: u32 = age.trim().parse() // LEGAL in rust to re-use the same variable name. Shadowing
@@ -11,4 +13,41 @@ fn main() {
     age = age + 1; // increment by 1.
 
     println!("I'm {} and I want ${} so I can buy some {}", age, ONE_MIL, PI);
+
+    println!("-------------------------------------------------------------------");
+    
+    // Integer Datatypes and their maximum value:
+    println!("Max i8: {}", i8::MAX);
+    println!("Max u8: {}", u8::MAX);
+    println!("Max i16: {}", i16::MAX);
+    println!("Max u16: {}", u16::MAX);
+    println!("Max i32: {}", i32::MAX);
+    println!("Max u32: {}", u32::MAX);
+    println!("Max i64: {}", i64::MAX);
+    println!("Max u64: {}", u64::MAX);
+    println!("Max i128: {}", i128::MAX);
+    println!("Max u128: {}", u128::MAX);
+    println!("Max isize: {}", isize::MAX);
+    println!("Max usize: {}", usize::MAX);
+
+    // Some float datatype and their max
+    println!("-------------------------------------------------------------------");
+    println!("Max f32: {}", f32::MAX);
+    println!("Max f64: {}", f64::MAX);
+    println!("-------------------------------------------------------------------");
+
+    // Booleans can have for value: true or false 
+    let _unused_bool_as_place_holder = true; // start with _ so compiler ignore
+    let _another_bool: bool = false; // explicit type annotation 
+    
+    
+    // characters are defined using single quote ''
+    let _my_grade = 'A'; // implicitly infer by compiler to be char type
+    let _min_grade: char = 'F'; // with explicit type annotation
+    let mut unassigned_char: char; // var definition, need explicit type
+    unassigned_char = '🫂'; // character is 4 bytes in Rust, encoded in Unicode Scalar Value.
+    println!("people hugging emoji: {}", unassigned_char);
+    unassigned_char = '🍎';
+    println!("red apple emoji: {}", unassigned_char);
+    
 }
