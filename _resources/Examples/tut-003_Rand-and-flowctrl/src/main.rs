43be65bb85159c2e65a518282aad9ee8835f4ee4
Richelin Metellus <richelin.metellus@outlook.com>
2025-04-25 12:13:23 -0500

    stop using depracted random number generator function

--- _resources/Examples/tut-003_Rand-and-flowctrl/src/main.rs
+++ _resources/Examples/tut-003_Rand-and-flowctrl/src/main.rs
@@ -2,6 +2,6 @@ use rand::Rng;
 
 fn main() {
     // generate a random int between 1 and 100 (inclusive)
-    let random_num = rand::thread_rng().gen_range(1..101);
+    let random_num = rand::rng().random_range(1..101);
     println!("Random num: {}", random_num);
 }
