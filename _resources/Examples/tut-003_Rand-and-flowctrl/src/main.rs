78391949f68ee717a31fb8e9889bba9f83682c85
Richelin Metellus <richelin.metellus@outlook.com>
2025-04-25 12:08:54 -0500

    code as provided by teacher but with warnings

--- _resources/Examples/tut-003_Rand-and-flowctrl/src/main.rs
+++ _resources/Examples/tut-003_Rand-and-flowctrl/src/main.rs
@@ -1,3 +1,7 @@
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    // generate a random int between 1 and 100 (inclusive)
+    let random_num = rand::thread_rng().gen_range(1..101);
+    println!("Random num: {}", random_num);
 }
