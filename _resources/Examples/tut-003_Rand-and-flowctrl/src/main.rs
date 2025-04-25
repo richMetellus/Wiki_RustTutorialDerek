f00dec0a1338b817be431c37691e342ad50b9eac
Richelin Metellus <richelin.metellus@outlook.com>
2025-04-25 12:25:10 -0500

    added how to do if, else if, else in Rust

--- _resources/Examples/tut-003_Rand-and-flowctrl/src/main.rs
+++ _resources/Examples/tut-003_Rand-and-flowctrl/src/main.rs
@@ -2,6 +2,18 @@ use rand::Rng;
 
 fn main() {
     // generate a random int between 1 and 100 (inclusive)
-    let random_num = rand::rng().random_range(1..101);
-    println!("Random num: {}", random_num);
+    let age = rand::rng().random_range(1..101);
+    println!("Random age: {}", age);
+    println!("-------------------------------------------------------------------");
+
+    if (age >= 1) && (age <= 18){
+        println!("Important Birthday");
+    } else if (age == 21) || (age == 50){
+        println!("{} is Important Birthday", age);
+    } else if age >= 65 {
+        println!("{} is Important Birthday", age);
+    } else {
+        println!("{} is Not an Important Birthday", age);
+    }
+
 }
