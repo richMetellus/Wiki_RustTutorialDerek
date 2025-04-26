b83c81c6a188a1eb6bfa0394a378d617ec365710
Richelin Metellus <richelin.metellus@outlook.com>
2025-04-26 13:49:13 -0500

    added examples of array and memory size occupied by array

--- _resources/Examples/tut-004_arrays-loops-tuples/src/main.rs
+++ _resources/Examples/tut-004_arrays-loops-tuples/src/main.rs
@@ -1,3 +1,24 @@
+use std::mem; 
+
 fn main() {
-    println!("Hello, world!");
+    
+    // ----- ARRAYS -----
+    // Elements in an array must be of the same type
+    // and have a fixed size
+    const ARR_1: [i32; 4] = [1,2,3,4];
+    let arr1_ref: &[i32] = &ARR_1;
+    // Get value by index starting at 0
+    println!("1st : {}", ARR_1[0]);
+    println!("2nd element : {}", ARR_1[1]);
+
+    // Get array length
+    println!("Length : {}", ARR_1.len());
+    
+    // Get the size (bytes) of the array.. arrays are continuous block of memory
+    println!("total size of memory occupied by 1 i32 : {} bytes", mem::size_of::<i32>());
+    println!("total size of memory occupied by ARR_1 : {} bytes", mem::size_of_val(&ARR_1));
+    println!("total size of memory occupied by ARR_1 using arr1_ref: {} bytes", mem::size_of_val(arr1_ref));
+    println!("-------------------------------------------------------------------");
+
+
 }
--- relevant_urls.rst
+++ relevant_urls.rst
@@ -8,3 +8,5 @@
 .. _Learn Rust (rust website): https://www.rust-lang.org/learn
 .. _crates.io: https://crates.io/
 .. _Rust By Example - crates: https://doc.rust-lang.org/rust-by-example/crates.html
+.. _Array Size Pass by Macro (forum): https://users.rust-lang.org/t/array-size-as-placeholder-to-dynamic-allocate-size/87174/11
+.. _Dynamic size array and rust (reddit): https://www.reddit.com/r/rust/comments/3gp0tl/dynamic_fixed_sized_arrays/
