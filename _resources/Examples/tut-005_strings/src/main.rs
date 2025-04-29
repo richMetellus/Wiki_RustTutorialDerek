f3f3fa12525005ddf27b84ad47ba754d72903329
Richelin Metellus <richelin.metellus@outlook.com>
2025-04-29 08:51:23 -0500

    mutable string String type

--- _resources/Examples/tut-005_strings/src/main.rs
+++ _resources/Examples/tut-005_strings/src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
-    println!("Hello, world!");
+    println!("-------------------------------------------------------------------");
+    // Create an empty growable string
+    let mut st1 = String::new();
+
+    // Insert a character at the end of a string
+    st1.push('A');
+
+    // Insert a string at the end
+    st1.push_str(" word"); // notice the space
+
+    // Iterate through words by splitting at whitespace
+    println!("Splitting st1 = \"{st1}\" - using whitespace delimiter");
+    for word in st1.split_whitespace() {
+        println!("{}", word);
+    }
+
+    // Replace "A" from st1 to Another and bind the value to st2.
+    println!("String substitution:  Replacing 'A' with \"Another\" in st1 = \"{st1}\" ");
+    let st2 = st1.replace("A", "Another");
+    println!("st2 = \"{}\"", st2);
+    println!("is st1 still in scope? Yes; st1 ={st1}");
 }
