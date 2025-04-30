e91415bd985fa4d82b87cc07131d8da789ba58aa
Richelin Metellus <richelin.metellus@outlook.com>
2025-04-30 17:40:30 -0500

    finish lesson 6, vectors section added

--- _resources/Examples/tut-006_casting-enums-vectors/src/main.rs
+++ _resources/Examples/tut-006_casting-enums-vectors/src/main.rs
@@ -56,5 +56,51 @@ fn main() {
     println!("Is yesterday the weekend? {}", yesterday.is_weekend());
     println!("-------------------------------------------------------------------");
 
+    // ----- VECTORS -----
+    // Vectors are like arrays that can grow if mutable
+    // They only store values of the same type
+
+    // Create an empty vector with i32
+    let _vec1: Vec<i32> = Vec::new();
+    println!("empty vector created, but unused");
+
+    // Create a vector with defined values
+    let mut vec2 = vec![1, 2, 3, 4];
+
+    println!("vector 2 created with values: vec2 = [1, 2, 3, 4]");
+
+    // Add values to the end of a vector
+    vec2.push(5);
+
+    println!("value 5 is pushed to vec2\n --Getting element via index and `get`");
+
+    // Get value by index
+    println!("1st : {}", vec2[0]);
+
+    // Verify value exists
+    let _second_element: &i32 = &vec2[1];
+    match vec2.get(1) {
+        Some(second) => println!("2nd : {}", second),
+        None => println!("No 2nd value"),
+    };
+
+    // Cycle and change values
+    println!("Doubling each value of element in mutable vector 2, then print");
+    for i in &mut vec2 {
+        *i *= 2;
+    }
+
+    // Cycle through vector values
+    for i in &vec2 {
+        println!("{}", i);
+    }
+
+    // Get number of values in a vector
+    println!("Vec Length : {}", vec2.len());
+
+    println!("Remove and return the last value in vec2");
+    println!("Pop {:?}", vec2.pop());
+    println!("Vec Length : {}", vec2.len());
+
 
 }
