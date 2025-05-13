a0ab9181ec0c36932f10be5c9481db66653066af
Richelin Metellus <richelin.metellus@outlook.com>
2025-05-12 20:11:55 -0500

    added a bad example of generics

--- _resources/Examples/tut-007_functions-generics/src/main.rs
+++ _resources/Examples/tut-007_functions-generics/src/main.rs
@@ -37,6 +37,11 @@ fn sum_list(list: &[i32]) -> i32 {
     sum
 }
 
+// We get 2 generic types of the same type and return that same type
+fn get_sum_gen<T>(x: T, y: T) -> T {
+    return x + y;
+}
+
 fn main() {
     // ----- FUNCTIONS calls -----
     say_hello();
@@ -51,4 +56,12 @@ fn main() {
 
     let num_list = vec![1,2,3,4,5];
     println!("Given a list [1,2,3,4,5] \nSum of list = {}", sum_list(&num_list));
+
+    // ----- GENERIC TYPES -----
+    // We can specify the data type to be used at a later time with generics
+    // It is mainly used when we want to create functions that can work with
+    // multiple data types. It is used with structs, enums, traits, etc.
+    // which we'll talk about later
+    println!("5 + 4 = {}", get_sum_gen(5,4));
+    println!("5.2 + 4.6 = {}", get_sum_gen(5.2,4.6));
 }
