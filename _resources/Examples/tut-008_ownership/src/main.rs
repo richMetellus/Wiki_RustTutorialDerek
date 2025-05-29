247c120bb72706eabfa7438079922fd4f0784eaf
Richelin Metellus <richelin.metellus@outlook.com>
2025-05-29 16:28:30 -0500

    demonstrated error of borrow of move value in rust

---
+++ _resources/Examples/tut-008_ownership/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    // create 2 strings
+    let str1: String = String::from("World");
+    let str2: String = str1;
+    
+    println!("Hello {}", str2);
+
+    //Uncomment this and try to print str1 after assignment (move)
+    // println!("Hello {}", str1);
+
+}
