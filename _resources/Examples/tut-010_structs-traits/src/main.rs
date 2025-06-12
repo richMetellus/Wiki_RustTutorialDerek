6af656048decca56806ddbbe024cb1a03447340c
Richelin Metellus <richelin.metellus@outlook.com>
2025-06-11 21:53:22 -0500

    Showed example of struct with generic data fields

--- _resources/Examples/tut-010_structs-traits/src/main.rs
+++ _resources/Examples/tut-010_structs-traits/src/main.rs
@@ -1,3 +1,4 @@
+#![allow(unused_variables)]
 fn main() {
 
     // ----- STRUCTS -----
@@ -21,6 +22,18 @@ fn main() {
     // say Bob moves and you want to update his address
     bob.address = String::from("505 Main St"); // since Bob is mutable we can update the struct fields.
     println!("Address : {}", bob.address);
+
+    // You could accept multiple data types using generics like
+    // we did with functions. If we had a GenericRectangle struct
+    // that could accept floats or ints we would need 2 generics
+    struct GenericRectangle<T, U> {
+        length: T, 
+        height: U,
+    }
+
+    // define a struct
+    let rec = GenericRectangle { length: 4, height: 10.5} ;
+    println!("GenericRectangle length : {}, height: {}", rec.length, rec.height);
 
 
 }
