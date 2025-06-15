4449631ebc514f6a5d1d297f5bb2cb5bb3c09709
Richelin Metellus <richelin.metellus@outlook.com>
2025-06-15 11:15:00 -0500

    try index out of bounds example

--- _resources/Examples/tut-012_io-and-error-handling/src/main.rs
+++ _resources/Examples/tut-012_io-and-error-handling/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    panic!("An unrecoverable error has occurred");
+    // ----- READING & WRITING TO FILES & ERROR HANDLING -----
+    // Rust doesn't have exceptions like other languages. It handles
+    // recoverable errors with Result and the panic! macro for
+    // unrecoverable errors
+
+    // When the panic! macro executes your program prints an error
+    // memory is cleaned up and the program quits
+    // panic!("Terrible Error");
+
+    // Accessing an index that doesn't exist calls panic
+    // let lil_arr = [1,2];
+    // println!("{}", lil_arr[10]);
+
 }
