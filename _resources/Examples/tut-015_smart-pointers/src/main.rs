b523c857fe60f213ff495f3ac8b337a140f358a8
Richelin Metellus <richelin.metellus@outlook.com>
2025-07-04 23:45:44 -0500

    added an example of binary tree

--- _resources/Examples/tut-015_smart-pointers/src/main.rs
+++ _resources/Examples/tut-015_smart-pointers/src/main.rs
@@ -39,16 +39,73 @@ fn main() {
     // This is saying we can't include nodes in a node because
     // the size of node depends on the size of multiple nodes
     // which confuses the compiler
-    struct TreeNode<T> {
-        pub left: TreeNode<T>,
-        pub right: TreeNode<T>,
-        pub key: T, // this caused error
-    }
+    // struct TreeNode<T> {
+    //     pub left: TreeNode<T>,
+    //     pub right: TreeNode<T>,
+    //     pub key: T, // this caused error
+    // }
 
     // We have other problems in that Binary Trees eventually end
     // and Rust doesn't like Null values so we have to use Option
 
     // We can use a Box here because it has a pointer to data and
     // a fixed size
+
+    struct TreeNode<T> {
+        pub left: Option<Box<TreeNode<T>>>,
+        pub right: Option<Box<TreeNode<T>>>,
+        pub key: T
+    }
+
+    // Create functions for creating nodes and adding left & right
+    impl<T> TreeNode<T> {
+        // the key is the value assigned to the node.
+        pub fn new(key: T) -> Self {
+            TreeNode {
+                left: None, // bring new node, so typically they have none.
+                right: None,
+                key,
+            }
+        }
+        // to assign a left tree node.
+        pub fn left(mut self, node: TreeNode<T>) -> Self {
+            self.left = Some(Box::new(node));
+            self
+        }
+        // to assign a right tree node.
+        pub fn right(mut self, node: TreeNode<T>) -> Self {
+            self.right = Some(Box::new(node));
+            self
+        }
+    }
+
+    // Used to test original, single node binary tree.
+    let mut boss = TreeNode {
+        left: None,
+        right: None,
+        key: 50,
+    };
+
+    // Create the root node with left and right
+
+    /*
+             1
+            / \
+           2    3
+          / \  / \
+          4 5  6  7
+     */
+
+    let node2 = TreeNode::new(2)
+    .left(TreeNode::new(4))
+    .right(TreeNode::new(5));
+    
+    let node3 = TreeNode::new(3)
+    .left(TreeNode::new(6))
+    .right(TreeNode::new(7));
+
+    let binary_tree1 = TreeNode::new(1)
+    .left(node2)
+    .right(node3);
 
 }
