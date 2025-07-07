d1497b96453ffe07ef5dffcb5cf322f911919f9c
Richelin Metellus <richelin.metellus@outlook.com>
2025-07-07 12:17:55 -0500

    added fix so thread finish completion.

--- _resources/Examples/tut-016-concurrency/src/main.rs
+++ _resources/Examples/tut-016-concurrency/src/main.rs
@@ -16,7 +16,7 @@ fn main() {
     // Create a thread with spawn which takes a closure.
     // This thread will show how a thread does not necessarily to everything in
     // time.
-    thread::spawn(|| {
+    let thread1 = thread::spawn(|| {
         for i in 1..25 {
             // this tread cycle through and share outputting information to the screen
             println!("Spawned thread : {}", i);
@@ -31,5 +31,9 @@ fn main() {
         println!("Main thread : {}", count);
         thread::sleep(Duration::from_millis(1));
     }
+
+    // We call join here so that the main thread executes with thread1
+    // unwrap handles the option Result which is Ok or Err
+    thread1.join().unwrap();
 
 }
