a399b7ba40c617cc1f18530d969f939a21696341
Richelin Metellus <richelin.metellus@outlook.com>
2025-07-07 11:54:30 -0500

    started transcribing section on thread in rust

---
+++ _resources/Examples/tut-016-concurrency/src/main.rs
@@ -0,0 +1,35 @@
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    // ----- CONCURRENCY -----
+    // Concurrent programming involves executing different blocks of code
+    // independently, while parallel programming is when different
+    // code executes at the same instant of time. A thread handles scheduling
+    // and execution of these blocks of code.
+
+    // Common problems with parallel programming involve :
+    // 1. Thread are accessing data in the wrong order
+    // 2. Threads are blocked from executing because of confusion
+    // over requirements to proceed with execution
+
+    // Create a thread with spawn which takes a closure.
+    // This thread will show how a thread does not necessarily to everything in
+    // time.
+    thread::spawn(|| {
+        for i in 1..25 {
+            // this tread cycle through and share outputting information to the screen
+            println!("Spawned thread : {}", i);
+            // Forces thread to sleep and allow another thread like main thread to execute
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    // There are no guarantees on when the threads will execute and
+    // that they will complete execution
+    for count in 1..20 {
+        println!("Main thread : {}", count);
+        thread::sleep(Duration::from_millis(1));
+    }
+
+}
